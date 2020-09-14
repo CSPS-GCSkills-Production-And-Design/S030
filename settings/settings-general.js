a1f005b759e38a1148f1b5331f1a3a2f7e32c2ef
csabourin <christian.sabourin@canada.ca>
2020-09-14 14:24:25 -0400

    First commit

---
+++ settings/settings-general.js
@@ -0,0 +1,12 @@
+'use strict';
+define({
+  "courseLegacyCode": "X123",
+  "courseTitle_en": "ICS - 100",
+  "courseTitle_fr": "SCI - 100",
+  "courseSubtitle_en": "Introduction to Incident Command System",
+  "courseSubtitle_fr": "Introduction au Système de Commandement d'Intervention",
+  "seriesTitle_en": "Introduction to Incident Command System",
+  "seriesTitle_fr": "Introduction au Système de Commandement d'Intervention",
+  "addToolboxPage": [],
+  "editMode": true
+});
